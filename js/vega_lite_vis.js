f61c0bc4f331ccec2be77520d233f77169163bb6
Hew Kai Feng <khew0006@student.monash.edu>
2026-05-24 02:59:31 +0800

    done bump chart

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -3,6 +3,7 @@ var heatmap = "charts/chart_heatmap.vg.json";
 var stacked_area = "charts/chart_area_multiples.vg.json";
 var dumbbell = "charts/chart_dumbbell.vg.json";
 var lollipop = "charts/chart_lollipop.vg.json";
+var bump = "charts/chart_bump.vg.json";
 var slope = "charts/chart_slope.vg.json";
 
 vegaEmbed("#choropleth_map", choropleth_map, {"actions": false})
@@ -21,7 +22,11 @@ vegaEmbed("#dumbbell", dumbbell, {"actions": false})
   .then(function(result){})
   .catch(console.error);
 
-vegaEmbed("#lollipop", lollipop, {"actions": false})
+vegaEmbed("#lollipop", lollipop, {"actions": false, "width": "container"})
+  .then(function(result){})
+  .catch(console.error);
+
+vegaEmbed("#bump", bump, {"actions": false, "width": "container"})
   .then(function(result){})
   .catch(console.error);
 
